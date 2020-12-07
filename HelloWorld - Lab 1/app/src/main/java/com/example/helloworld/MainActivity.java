c7755a6bc6e89de56cb1b62808660d97577f0b85
arif teguh wangi <arifteguhwangi@gmail.com>
2020-12-07 09:49:57 +0700

    add native c function to hello world

--- HelloWorld - Lab 1/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld - Lab 1/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,11 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.loadLibrary("hello-jni");
+        String test = stringFromJNI(20);
+        TextView main_text =  (TextView)findViewById(R.id.main_text);
+        main_text.setText(test);
+
     }
+
+    public native String stringFromJNI(int x);
 
     public int add(int a, int b) {
         return a + b;
     }
+
+
+
+
+
+
 
 }
 
